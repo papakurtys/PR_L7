1cbc00d04bba4963aa419a12c2557189db85a86f
Konrad Kurtyka <kondziok10@o2.pl>
2021-05-04 18:43:53 +0200

    Dodanie Routingu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
 import Posts from "./components/posts";
+import NotFound from "./components/notFound";
+import Home from "./components/home";
+import NavBar from "./components/common/navbar";
 
 function App() {
-
-
-  return (
-    <Posts/>
-  );
+ return (
+     <div className="container-fluid">
+       <NavBar />
+       <div className="container">
+           <div className="content">
+               <Switch>
+                   <Route path="/posts"
+                   component={Posts} />}
+                   />
+                   <Route path="/not-found" component={NotFound} />
+                   <Route path="/" exact component={Home} />
+                   <Redirect to="/not-found" />
+               </Switch>
+           </div>
+       </div>
+     </div>
+ );
 }
 
 export default App;
