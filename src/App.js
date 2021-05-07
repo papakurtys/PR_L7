3f5587d61a3f583a2bfcb7f8ffbb3a2f62b4a6b7
Konrad Kurtyka <kondziok10@o2.pl>
2021-05-07 13:49:59 +0200

    dodanie form login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Posts from "./components/posts";
 import NotFound from "./components/notFound";
 import Home from "./components/home";
 import NavBar from "./components/common/navbar";
+import LoginForm from "./components/loginForm";
 
 function App() {
  return (
@@ -12,9 +13,8 @@ function App() {
        <div className="container">
            <div className="content">
                <Switch>
-                   <Route path="/posts"
-                   component={Posts} />}
-                   />
+                   <Route path="/login" component={LoginForm} />
+                   <Route path="/posts" component={Posts} />} />
                    <Route path="/not-found" component={NotFound} />
                    <Route path="/" exact component={Home} />
                    <Redirect to="/not-found" />
